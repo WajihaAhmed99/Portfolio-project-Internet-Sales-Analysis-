67ecf301c16f74708d1d8f0d9938ae97ea5d2258
WajihaAhmed99 <132120179+WajihaAhmed99@users.noreply.github.com>
2023-04-28 21:35:40 -0700

    Initial commit

---
+++ Data GitHub/SQLQuery2.customer.sql
@@ -0,0 +1,38 @@
+-- Cleanesed DIM_Customer Table--
+SELECT 
+		c.customerkey AS CustomerKey,
+      --[GeographyKey],
+      --[CustomerAlternateKey],
+      --[Title],
+      c.firstname AS [First Name],
+      --[MiddleName],
+      c.lastname AS [Last Name],
+	  c.firstname + ' ' + lastname AS [Full Name],
+	  -- Combine first and last name 
+      --[NameStyle],
+      --[BirthDate],
+      --[MaritalStatus],
+      --[Suffix],
+      CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender,
+      --[EmailAddress],
+      --[YearlyIncome],
+      --[TotalChildren],
+      --[NumberChildrenAtHome],
+      --[EnglishEducation],
+      --[SpanishEducation],
+      --[FrenchEducation],
+      --[EnglishOccupation],
+      --[SpanishOccupation],
+      --[FrenchOccupation],
+      --[HouseOwnerFlag],
+      --[NumberCarsOwned],
+      --[AddressLine1],
+      --[AddressLine2],
+      --[Phone],
+      c.datefirstpurchase AS DateFirstPurchase,
+      --[CommuteDistance],
+	  g.city AS [Customer city] --join city from geography table
+  FROM [AdventureWorksDW2019].[dbo].[DimCustomer] as c
+  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey
+  ORDER BY 
+  CustomerKey ASC -- Order list by CustomerKey
